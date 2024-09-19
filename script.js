0d8c0c179cbc5a4f09054066fee3f5f0e3963f9d
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-19 16:03:49 -0300

    Add operate() function

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function divide(a, b) {
 let numberA = 0;
 let numberB = 0;
 let operator = "";
+
+function operate(operator, a, b) {
+    let result = 0;
+
+    switch(operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
+}
